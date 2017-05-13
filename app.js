5f3eb299555bf3fce601b5f9ec58c42078ed28c7
Lilja <triforce@windowslive.com>
2017-05-13 15:39:59 +0300

    front page and hello world page

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var bodyParser = require('body-parser');
 //var mongo = require('mongodb');
 //var monk = require('monk');
 //var db = monk('localhost:27017/nodetest1');
-var index = require('./routes/index');
-//var quotes = require('./routes/quotes');
+
+var routes = require('./routes/index');
+var quotes = require('./routes/quotes');
 
 var app = express();
 
@@ -27,7 +28,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 //    next();
 //});
 
-app.use('/', index);
-//app.use('/quotes', quotes);
+app.use('/', routes);
+app.use('/quotes', quotes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 module.exports = app;
