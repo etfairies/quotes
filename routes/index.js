17042e40d1af07a527793a3b17e9b2490b79867c
Lilja <triforce@windowslive.com>
2017-05-16 21:08:50 +0300

    save quote to/get quotes from database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb');
+var db;
+
+MongoClient.connect('mongodb://etfairies:etfairies@ds143221.mlab.com:43221/heroku_lkqmfpvw', function (err, database) {
+    if (err) return console.log(err);
+    db = database;
+});
 
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Quotes'});
@@ -7,23 +14,31 @@ router.get('/', function (req, res, next) {
 
 router.get('/quotes', function (req, res) {
 
-    var quotelist = [
-        {
-            'author': 'Aristotle',
-            'description': 'It is during our darkest moments that we must focus to see the light.'
-        },
-        {
-            'author': 'Edmund Burke',
-            'description': 'The only thing necessary for the triumph of evil is for good men to do nothing.'
-        },
-        {
-            'author': 'William Arthur Ward',
-            'description': 'The pessimist complains about the wind; the optimist expects it to change; the realist adjusts the sails.'
-        }
-    ];
-
-    res.render('quotelist', {
-        "quotelist": quotelist
+//    var quotelist = [
+//        {
+//            'author': 'Aristotle',
+//            'description': 'It is during our darkest moments that we must focus to see the light.'
+//        },
+//        {
+//            'author': 'Edmund Burke',
+//            'description': 'The only thing necessary for the triumph of evil is for good men to do nothing.'
+//        },
+//        {
+//            'author': 'William Arthur Ward',
+//            'description': 'The pessimist complains about the wind; the optimist expects it to change; the realist adjusts the sails.'
+//        }
+//    ];
+//
+//    res.render('quotelist', {
+//        "quotelist": quotelist
+//    });
+    
+//    var cursor = db.collection('quotes').find();
+    
+    db.collection('quotes').find().toArray(function(err, results) {
+            res.render('quotelist', {
+            "quotelist": results
+        });
     });
 });
 
@@ -33,6 +48,15 @@ router.get('/addquote', function (req, res) {
 
 router.post('/addquote', function (req, res) {
     console.log(req.body);
+    
+    db.collection('quotes').save(req.body, function (err, result) {
+        if (err) {
+            return console.log(err);
+        }
+        
+        console.log('saved to database');
+    })
+    
     res.redirect("/quotes");
 //    var db = req.db;
 //    
