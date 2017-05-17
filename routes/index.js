8e5bdd19a25cd659fca60fb7336ab012e99aad7b
Lilja <triforce@windowslive.com>
2017-05-17 15:30:02 +0300

    added 'likes' to quotes

--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,49 @@ var router = express.Router();
 var MongoClient = require('mongodb');
 var db;
 
+// Connect  to the database
 MongoClient.connect('mongodb://etfairies:etfairies@ds143221.mlab.com:43221/heroku_lkqmfpvw', function (err, database) {
     if (err)
         return console.log(err);
     db = database;
 });
 
+// Get index page
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Quotes'});
 });
 
+// List all quotes from the database
 router.get('/quotes', function (req, res) {
     db.collection('quotes').find().toArray(function (err, results) {
+        if (err) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        }
+        
         res.render('quotelist', {
             "quotelist": results
         });
     });
 });
 
+// Get 'add quote' page 
 router.get('/addquote', function (req, res) {
     res.render('newquote', {title: 'Add New Quote'});
 });
 
+// Add new quote to database
 router.post('/addquote', function (req, res) {
-     db.collection('quotes').save(req.body, function (err, result) {
+    db.collection('quotes').save(req.body, function (err, result) {
         if (err) {
-            return console.log(err);
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
         }
     });
     res.redirect("/quotes");
