fe9b3e5c81f6368d81ba290bf2606e4ddf8382b6
Lilja <triforce@windowslive.com>
2017-05-19 19:26:57 +0300

    added some tests

--- test/quote_test.js
+++ test/quote_test.js
@@ -3,14 +3,29 @@ var chaiHttp = require('chai-http');
 var server = require('../bin/www');
 var should = chai.should();
 var expect = chai.expect;
+var MongoClient = require('mongodb');
+var ObjectId = require('mongodb').ObjectId;
 
 chai.use(chaiHttp);
 
 
-
-// Tests
-
 describe('Quotes', function () {
+    // Database connection
+    var database = null;
+
+    before(function (done) {
+        MongoClient.connect('mongodb://etfairies:etfairies@ds143071.mlab.com:43071/heroku_nt73w1pp').then(function (db) {
+            database = db;
+            return database.dropDatabase();
+        }).then(function () {}).then(done, done);
+    });
+
+    afterEach(function (done) {
+        database.dropDatabase().then(function () {}).then(done, done);
+    });
+
+    //Tests
+
     it('should get index page on / GET', function (done) {
         chai.request(server)
                 .get('/')
@@ -20,18 +35,26 @@ describe('Quotes', function () {
                 });
     });
 
-    it('should list all quotes on /quotes GET');//, function (done) {
-//        chai.request(server)
-//                .get('/quotes')
-//                .end(function (err, res) {
-//                    res.should.have.status(200);
-//                    res.should.be.json;
-//                    res.body.should.be.a('array');
-//                    done();
-//                });
-//   });
+    it('should list all quotes on /quotes GET', function (done) {
 
-    it('should add a single quote on /addquote POST');
+        chai.request(server)
+                .get('/quotes')
+                .end(function (err, res) {
+                    res.should.have.status(200);
+//                    res.body.should.be.a('array');
+                    done();
+                });
+    });
+
+    it('should add a single quote on /addquote POST', function (done) {
+        chai.request(server)
+                .get('/addquote')
+                .end(function (err, res) {
+                    res.should.have.status(200);
+                    done();
+                })
+    });
+
     it('should update a single quote on /quotes/like POST');
     it('should delete a single quote on /quotes/delete POST');
 });
