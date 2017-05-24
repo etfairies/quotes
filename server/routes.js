e93c00de66332e4ac5cfc3e0ddf7d4fcd8bf4fb7
Lilja <triforce@windowslive.com>
2017-05-24 16:39:36 +0300

    deleted old files and renamed some files

--- server/routes.js
+++ server/routes.js
@@ -13,33 +13,30 @@ MongoClient.connect('mongodb://etfairies:etfairies@ds143221.mlab.com:43221/herok
 
 // Get index page
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Quotes'});
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Hello from the custom server!"}');
 });
 
 // List all quotes from the database
 router.get('/quotes', function (req, res) {
     db.collection('quotes').find().toArray(function (err, results) {
+        res.set('Content-Type', 'application/json');
+
         if (err) {
             res.status(err.status || 500);
-            res.render('error', {
-                message: err.message,
-                error: err
-            });
         }
-
-        res.render('quotelist', {
-            "quotelist": results
-        });
+        var quotelist = {"quotes": results};
+        res.send(quotelist);
     });
 });
 
 // Get 'add quote' page 
-router.get('/addquote', function (req, res) {
+router.get('/add', function (req, res) {
     res.render('newquote', {title: 'Add New Quote'});
 });
 
 // Add new quote to database
-router.post('/addquote', function (req, res) {
+router.post('/add', function (req, res) {
     var quote = {
         author: req.body.author,
         description: req.body.description,
@@ -51,7 +48,7 @@ router.post('/addquote', function (req, res) {
 });
 
 // Increment 'likes' of given quote
-router.post('/quotes/like', function (req, res) {
+router.post('/like', function (req, res) {
     db.collection('quotes').update({_id: ObjectId(req.body.quoteid)}, {
         $inc: {likes: 1}
     });
@@ -60,7 +57,7 @@ router.post('/quotes/like', function (req, res) {
 });
 
 // Delete quote
-router.post('/quotes/delete', function (req, res) {
+router.post('/delete', function (req, res) {
     db.collection('quotes').remove({_id: ObjectId(req.body.quoteid)});
     res.redirect("/quotes");
 });
