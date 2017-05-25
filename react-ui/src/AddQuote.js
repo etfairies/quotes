201462158faad7aa39a38a864ee6e934eec749ea
Lilja <triforce@windowslive.com>
2017-05-25 18:52:20 +0300

    handle input change in page 'add quote'

--- react-ui/src/AddQuote.js
+++ react-ui/src/AddQuote.js
@@ -5,18 +5,29 @@ import './App.css';
 class AddQuote extends Component {
     constructor(props) {
         super(props);
+        this.state = {author: '', description: '' };
+        
         this.sendQuote = this.sendQuote.bind(this);
     }
 
     sendQuote(event) {
-
+        
+    }
+    
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+         this.setState({
+             [name]: value
+         });
     }
 
     render() {
         return(
                 <form onSubmit={this.sendQuote}>
-                    <input type="text" name="author" />
-                    <input type="text" name="description" />
+                    <input type="text" name="author" onChange={this.handleInputChange}/>
+                    <input type="text" name="description" onChange={this.handleInputChange}/>
                 </form>
                 );
     }
